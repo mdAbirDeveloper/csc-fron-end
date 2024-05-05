c251c2fcf87ca4acc6cf394786298b194cc5b91c
mdAbirDeveloper <mdabir.developer@gmail.com>
2024-05-05 17:57:55 +0600

    after fotter tuchup

--- src/pages/components/Footer.js
+++ src/pages/components/Footer.js
@@ -5,22 +5,22 @@ const Footer = () => {
     <div>
       <footer className="footer p-10 bg-neutral text-neutral-content">
         <nav>
-          <h6 className="footer-title">Services</h6>
-          <a className="link link-hover">Fog</a>
-          <a className="link link-hover">Mist</a>
+          <h6 className="footer-title">Head Office</h6>
+          <a className="link link-hover">Airport Road، Building 7, Level 1</a>
+          <a className="link link-hover">Zone A, The Business Gate, Riyadh</a>
         </nav>
         <nav>
-          <h6 className="footer-title">Company</h6>
-          <a className="link link-hover">About us</a>
-          <a className="link link-hover">Contact</a>
-          <a className="link link-hover">Jobs</a>
-          <a className="link link-hover">Press kit</a>
+          <h6 className="footer-title">Inquiries</h6>
+          <a className="link link-hover">For any inquiries, questions or commendations, please call:</a>
+          <a className="link link-hover">+966533360688 or fill out the following form</a>
+          <a className="link link-hover">info@cities-systems.sa</a>
+          <a className="link link-hover">Tel: +966505580899</a>
+          <a className="link link-hover">Fax: +966533360688</a>
         </nav>
         <nav>
-          <h6 className="footer-title">Legal</h6>
-          <a className="link link-hover">Terms of use</a>
-          <a className="link link-hover">Privacy policy</a>
-          <a className="link link-hover">Cookie policy</a>
+          <h6 className="footer-title">Employment</h6>
+          <a className="link link-hover">To apply for a job with Cities Systems Corporation,</a>
+          <a className="link link-hover"> please send a cover letter together with your C.V. to: info@cities-systems.sa</a>
         </nav>
       </footer>
     </div>
