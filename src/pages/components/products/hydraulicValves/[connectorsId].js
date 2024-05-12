bf2bdb239a59c289369dfc6523fafe001dc4f674
mdAbirDeveloper <mdabir.developer@gmail.com>
2024-05-13 00:30:29 +0600

    after fix hydrolic

--- src/pages/components/products/hydraulicValves/[connectorsId].js
+++ src/pages/components/products/hydraulicValves/[connectorsId].js
@@ -182,7 +182,7 @@ const Connectors = () => {
                     type="radio"
                     name={`rating-${product?._id}`}
                     value={value}
-                    className={`mask mask-star-2 bg-orange-400 ml-3 rating ${
+                    className={`mask mask-star-2 bg-orange-400 ml-3 ${
                       value <= (hoverRating || rating) ? "checked" : ""
                     }`}
                     onClick={() => handleRatingClick(value)}
