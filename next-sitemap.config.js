15df64b46c30ffb643805343863c1c243dd67df0
mdAbirDeveloper <mdabir.developer@gmail.com>
2024-05-15 23:17:11 +0600

    after add sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://csc-fron-end.vercel.app/', // Replace with your website URL
+    generateRobotsTxt: true, // (optional) Generates a `robots.txt` file
+    // ...other options
+  }
+  
